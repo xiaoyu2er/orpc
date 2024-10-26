54167bf104569224c7a5ac1feab1134077ff9cb0
dinwwwh <dinwwwh@gmail.com>
2024-10-26 15:03:16 +0700

    feat(transformer): openapi transformer support multipart/form-data

--- packages/transformer/src/openapi/transformer.ts
+++ packages/transformer/src/openapi/transformer.ts
@@ -1,27 +1,55 @@
 /// <reference lib="dom" />
 /// <reference lib="dom.iterable" />
 
+import { construct } from '../object/construct'
+import { crush } from '../object/crush'
 import type { Body, Transformer } from '../types'
+import { findDeepMatches } from '../utils/find-deep-matches'
 import { parseJSONSafely } from '../utils/parse-json-safely'
 
 export class OpenAPITransformer implements Transformer {
   serialize(payload: unknown): { body: Body; headers: Headers } {
     const headers = new Headers()
 
-    // TODO: support more data type
+    let body: string | FormData | Blob | undefined
 
-    const body = JSON.stringify(payload, (_, value) => {
-      if (value instanceof Set) {
-        return [...value]
+    const hasBlobs =
+      findDeepMatches((v) => v instanceof Blob, payload).values.length > 0
+
+    if (hasBlobs) {
+      if (payload instanceof Blob) {
+        body = payload
+        headers.set('Content-Type', payload.type || 'application/octet-stream')
+      } else {
+        /** If payload has blob but itself is not a blob that mean always payload is a object or array  */
+        const payloadAsAny = payload as any
+        body = new FormData()
+        for (const name in crush(payloadAsAny)) {
+          const value = payloadAsAny[name]!
+
+          if (typeof value === 'string' || typeof value === 'number') {
+            body.append(name, value.toString())
+          } else if (typeof value === 'boolean') {
+            body.append(name, value.toString())
+          } else if (value instanceof Blob) {
+            body.append(name, value)
+          }
+        }
       }
+    } else {
+      body = JSON.stringify(payload, (_, value) => {
+        if (value instanceof Set) {
+          return [...value]
+        }
 
-      if (value instanceof Map) {
-        return [...value.entries()]
-      }
+        if (value instanceof Map) {
+          return [...value.entries()]
+        }
 
-      return value
-    })
-    headers.set('Content-Type', 'application/json')
+        return value
+      })
+      headers.set('Content-Type', 'application/json')
+    }
 
     return { body, headers }
   }
@@ -46,7 +74,9 @@ export class OpenAPITransformer implements Transformer {
     }
 
     if (contentType.startsWith('multipart/form-data')) {
-      throw new Error('TODO: multipart/form-data is not implemented yet')
+      const form = await re.formData()
+      const data = construct([...form.entries()])
+      return data
     }
 
     return await re.blob()

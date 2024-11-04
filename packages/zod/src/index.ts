5850c88f26ab6b9698fcf53017c31c627215de46
dinwwwh <dinwwwh@gmail.com>
2024-11-04 10:09:23 +0700

    feat(zod): `oz.file().type()` for mime type validation

--- packages/zod/src/index.ts
+++ packages/zod/src/index.ts
@@ -1,16 +1,20 @@
 /// <reference lib="dom" />
 
+import wcmatch from 'wildcard-match'
 import {
   type CustomErrorParams,
+  type ZodEffects,
   type ZodType,
   type ZodTypeAny,
   type ZodTypeDef,
-  z,
+  custom,
 } from 'zod'
 
 export type CustomZodType = 'File' | 'Blob' | 'Invalid Date' | 'RegExp' | 'URL'
 
 const customZodTypeSymbol = Symbol('customZodTypeSymbol')
+
+const customZodFileMimeTypeSymbol = Symbol('customZodFileMimeTypeSymbol')
 
 type CustomParams = CustomErrorParams & {
   fatal?: boolean
@@ -22,22 +26,102 @@ export function getCustomZodType(
   return schema._def[customZodTypeSymbol] as CustomZodType | undefined
 }
 
+export function getCustomZodFileMimeType(
+  schema: ZodTypeAny,
+): string | undefined {
+  return schema._def[customZodFileMimeTypeSymbol] as string | undefined
+}
+
+function composeParams<T = unknown>(options: {
+  params?: string | CustomParams | ((input: T) => CustomParams)
+  defaultMessage?: string | ((input: T) => string)
+}): (input: T) => CustomParams {
+  return (val) => {
+    const defaultMessage =
+      typeof options.defaultMessage === 'function'
+        ? options.defaultMessage(val)
+        : options.defaultMessage
+
+    if (!options.params) {
+      return {
+        message: defaultMessage,
+      }
+    }
+
+    if (typeof options.params === 'function') {
+      return {
+        message: defaultMessage,
+        ...options.params(val),
+      }
+    }
+
+    if (typeof options.params === 'object') {
+      return {
+        message: defaultMessage,
+        ...options.params,
+      }
+    }
+
+    return {
+      message: options.params,
+    }
+  }
+}
+
 export function file(
-  options?: CustomParams,
-): ZodType<InstanceType<typeof File>, ZodTypeDef, InstanceType<typeof File>> {
-  const schema = z.instanceof(File, options)
+  params?: string | CustomParams | ((input: unknown) => CustomParams),
+): ZodType<InstanceType<typeof File>, ZodTypeDef, InstanceType<typeof File>> & {
+  type(
+    mimeType: string,
+    params?: string | CustomParams | ((input: unknown) => CustomParams),
+  ): ZodEffects<
+    ZodType<InstanceType<typeof File>, ZodTypeDef, InstanceType<typeof File>>,
+    InstanceType<typeof File>,
+    InstanceType<typeof File>
+  >
+} {
+  const schema = custom<InstanceType<typeof File>>(
+    (val) => val instanceof File,
+    composeParams({ params, defaultMessage: 'Input is not a file' }),
+  )
 
   Object.assign(schema._def, {
     [customZodTypeSymbol]: 'File' satisfies CustomZodType,
   })
 
-  return schema
+  return Object.assign(schema, {
+    type: (
+      mimeType: string,
+      params: string | CustomParams | ((input: unknown) => CustomParams),
+    ) => {
+      const isMatch = wcmatch(mimeType)
+
+      const refinedSchema = schema.refine(
+        (val) => isMatch(val.type.split(';')[0]!),
+        composeParams<InstanceType<typeof File>>({
+          params,
+          defaultMessage: (val) =>
+            `Expected a file of type ${mimeType} but got a file of type ${val.type || 'unknown'}`,
+        }),
+      )
+
+      Object.assign(refinedSchema._def, {
+        [customZodTypeSymbol]: 'File' satisfies CustomZodType,
+        [customZodFileMimeTypeSymbol]: mimeType,
+      })
+
+      return refinedSchema
+    },
+  })
 }
 
 export function blob(
-  options?: CustomParams,
+  params?: string | CustomParams | ((input: unknown) => CustomParams),
 ): ZodType<InstanceType<typeof Blob>, ZodTypeDef, InstanceType<typeof Blob>> {
-  const schema = z.instanceof(Blob, options)
+  const schema = custom<InstanceType<typeof Blob>>(
+    (val) => val instanceof Blob,
+    composeParams({ params, defaultMessage: 'Input is not a blob' }),
+  )
 
   Object.assign(schema._def, {
     [customZodTypeSymbol]: 'Blob' satisfies CustomZodType,
@@ -47,14 +131,12 @@ export function blob(
 }
 
 export function invalidDate(
-  options?: CustomParams,
+  params?: string | CustomParams | ((input: unknown) => CustomParams),
 ): ZodType<Date, ZodTypeDef, Date> {
-  const schema = z
-    .instanceof(Date, options)
-    .refine((v) => Number.isNaN(v.getTime()), {
-      message: 'Expected a invalid date but got a valid date',
-      ...options,
-    })
+  const schema = custom<Date>(
+    (val) => val instanceof Date && Number.isNaN(val.getTime()),
+    composeParams({ params, defaultMessage: 'Input is not an invalid date' }),
+  )
 
   Object.assign(schema._def, {
     [customZodTypeSymbol]: 'Invalid Date' satisfies CustomZodType,
@@ -66,7 +148,10 @@ export function invalidDate(
 export function regexp(
   options?: CustomParams,
 ): ZodType<RegExp, ZodTypeDef, RegExp> {
-  const schema = z.instanceof(RegExp, options)
+  const schema = custom<RegExp>(
+    (val) => val instanceof RegExp,
+    composeParams({ params: options, defaultMessage: 'Input is not a regexp' }),
+  )
 
   Object.assign(schema._def, {
     [customZodTypeSymbol]: 'RegExp' satisfies CustomZodType,
@@ -76,7 +161,10 @@ export function regexp(
 }
 
 export function url(options?: CustomParams): ZodType<URL, ZodTypeDef, URL> {
-  const schema = z.instanceof(URL, options)
+  const schema = custom<URL>(
+    (val) => val instanceof URL,
+    composeParams({ params: options, defaultMessage: 'Input is not a URL' }),
+  )
 
   Object.assign(schema._def, {
     [customZodTypeSymbol]: 'URL' satisfies CustomZodType,

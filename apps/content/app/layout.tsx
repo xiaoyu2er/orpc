3a1fd8ff500bb949024b976e5f3a3f73926c4949
unnoq <contact@unnoq.com>
2024-11-18 08:42:37 +0700

    build(content): config output to export

--- apps/content/app/layout.tsx
+++ apps/content/app/layout.tsx
@@ -13,7 +13,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       suppressHydrationWarning
     >
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider
+          search={{
+            options: {
+              type: 'static',
+            },
+          }}
+        >
+          {children}
+        </RootProvider>
       </body>
     </html>
   )

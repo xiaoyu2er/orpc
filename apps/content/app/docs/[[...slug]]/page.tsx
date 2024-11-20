1e9994462cd67a547b76d8a11c44285fcbee1277
unnoq <contact@unnoq.com>
2024-11-20 08:37:23 +0700

    feat(content): edit on Github button

--- apps/content/app/docs/[[...slug]]/page.tsx
+++ apps/content/app/docs/[[...slug]]/page.tsx
@@ -22,7 +22,16 @@ export default async function Page(props: {
   const MDX = page.data.body
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage
+      toc={page.data.toc}
+      full={page.data.full}
+      editOnGithub={{
+        owner: 'unnoq',
+        repo: 'orpc',
+        sha: 'main',
+        path: `apps/content/content/docs/${page.file.path}`,
+      }}
+    >
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>

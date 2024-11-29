bd66bad03f944b0a831bae0a03461e9409077626
unnoq — oss/acc <contact@unnoq.com>
2024-11-29 15:19:07 +0700

    docs: fix playgrounds on Stackblitz (#39)
    * polyfill
    
    * nextjs issues
    
    * docs

--- playgrounds/nextjs/src/orpc.ts
+++ playgrounds/nextjs/src/orpc.ts
@@ -1,39 +1,44 @@
 import type { z } from 'zod'
 import type { UserSchema } from './schemas/user'
 import { ORPCError, os } from '@orpc/server'
-import { cookies, headers } from 'next/headers'
+import './polyfill'
 
-const base = os.use(async (input, context, meta) => {
-  const headerList = await headers()
-  let user = headerList.get('Authorization')
-    ? { id: 'test', name: 'John Doe', email: 'john@doe.com' } satisfies z.infer<typeof UserSchema>
-    : undefined
+const base = os
+  .use(async (input, context, meta) => {
+    const start = Date.now()
 
-  if (!user) {
-    const cookieList = await cookies()
-    // do something with cookies
-    user = { id: 'test', name: 'John Doe', email: 'john@doe.com' }
-  }
-
-  return meta.next({
-    context: {
-      db: 'dummy:postgres',
-      user,
-    },
+    try {
+      return await meta.next({})
+    }
+    catch (e) {
+      console.error(e)
+      throw e
+    }
+    finally {
+      // eslint-disable-next-line no-console
+      console.log(`[${meta.path.join('/')}] ${Date.now() - start}ms`)
+    }
   })
-})
+  .use(async (input, context, meta) => {
+  // You can use headers or cookies here to create the user object:
+  // import { cookies, headers } from 'next/headers'
+  // const headerList = await headers();
+  // const cookieList = await cookies();
+  //
+  // These lines are commented out because Stackblitz has issues with Next.js headers and cookies.
+  // However, this works fine in a local environment.
 
-export const pub = base.use(async (input, context, meta) => {
-  const start = Date.now()
+    const user = { id: 'test', name: 'John Doe', email: 'john@doe.com' } satisfies z.infer<typeof UserSchema>
 
-  try {
-    return await meta.next({})
-  }
-  finally {
-    // eslint-disable-next-line no-console
-    console.log(`[${meta.path.join('/')}] ${Date.now() - start}ms`)
-  }
-})
+    return meta.next({
+      context: {
+        db: 'dummy:postgres',
+        user,
+      },
+    })
+  })
+
+export const pub = base
 
 export const authed = pub.use(async (input, context, meta) => {
   if (!context.user) {

46edf34f3fd7add35566bef2c5479ca74ab67b36
unnoq <contact@unnoq.com>
2024-12-27 13:34:44 +0700

    nuxt playground link to scalar

--- playgrounds/nuxt/app.vue
+++ playgrounds/nuxt/app.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <h1>ORPC Playground</h1>
+    <p>
+      You can visit the
+      {' '}
+      <a href="/scalar">Scalar API Reference</a>
+      {' '}
+      page.
+    </p>
+    <hr>
     <OrpcMutation />
     <OrpcQuery />
   </div>

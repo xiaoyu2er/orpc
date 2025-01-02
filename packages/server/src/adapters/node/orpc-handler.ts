7fc9bb0a33717ca515e00ad5da13317a3e358c19
unnoq <contact@unnoq.com>
2025-01-02 15:24:08 +0700

    improve

--- packages/server/src/adapters/node/orpc-handler.ts
+++ packages/server/src/adapters/node/orpc-handler.ts
@@ -3,7 +3,7 @@ import type { Router } from '../../router'
 import type { Context } from '../../types'
 import type { ORPCHandlerOptions } from '../fetch/orpc-handler'
 import type { ConditionalRequestHandler, RequestOptions } from './types'
-import { createRequestListener } from '@mjackson/node-fetch-server'
+import { createRequest, sendResponse } from '@mjackson/node-fetch-server'
 import { ORPC_HANDLER_HEADER, ORPC_HANDLER_VALUE } from '@orpc/shared'
 import { ORPCHandler as ORPCFetchHandler } from '../fetch/orpc-handler'
 
@@ -18,11 +18,13 @@ export class ORPCHandler<T extends Context> implements ConditionalRequestHandler
     return Boolean(request.headers[ORPC_HANDLER_HEADER]?.includes(ORPC_HANDLER_VALUE))
   }
 
-  handle(req: IncomingMessage, res: ServerResponse, ...[options]: [options: RequestOptions<T>] | (undefined extends T ? [] : never)): void {
-    const listener = createRequestListener((request) => {
-      return this.orpcFetchHandler.fetch(request, (options ?? {}) as Exclude<typeof options, undefined>)
-    }, options)
+  async handle(req: IncomingMessage, res: ServerResponse, ...[options]: [options: RequestOptions<T>] | (undefined extends T ? [] : never)): Promise<void> {
+    const request = createRequest(req, res, options)
 
-    return listener(req, res)
+    const response = await this.orpcFetchHandler.fetch(request, (options ?? {}) as Exclude<typeof options, undefined>)
+
+    await options?.beforeSend?.(req, res)
+
+    return await sendResponse(res, response)
   }
 }
--- packages/server/src/adapters/node/types.ts
+++ packages/server/src/adapters/node/types.ts
@@ -1,15 +1,19 @@
 /// <reference types="node" />
 
-import type { RequestListenerOptions } from '@mjackson/node-fetch-server'
+import type { RequestOptions as BaseRequestOptions } from '@mjackson/node-fetch-server'
 import type { HTTPPath } from '@orpc/contract'
+import type { Promisable } from '@orpc/shared'
 import type { IncomingMessage, ServerResponse } from 'node:http'
 import type { Context, WithSignal } from '../../types'
 
 export type RequestOptions<T extends Context> =
+  & BaseRequestOptions
   & WithSignal
   & { prefix?: HTTPPath }
   & (undefined extends T ? { context?: T } : { context: T })
-  & RequestListenerOptions
+  & {
+    beforeSend?: (request: IncomingMessage, response: ServerResponse) => Promisable<void>
+  }
 
 export interface RequestHandler<T extends Context> {
   handle: (req: IncomingMessage, res: ServerResponse, ...opt: [options: RequestOptions<T>] | (undefined extends T ? [] : never)) => void

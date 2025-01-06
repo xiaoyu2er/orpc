aa720970f817c3f5dc069858e0a1966400f187a6
unnoq <contact@unnoq.com>
2025-01-06 17:04:23 +0700

    feat(server, openapi): hono adapter (#75)
    * config
    
    * hono middleware
    
    * docs
    
    * improve
    
    * fix types

--- packages/shared/src/value.ts
+++ packages/shared/src/value.ts
@@ -1,10 +1,10 @@
 import type { Promisable } from 'type-fest'
 
-export type Value<T> = T | (() => Promisable<T>)
+export type Value<T, TArgs extends any[] = []> = T | ((...args: TArgs) => Promisable<T>)
 
-export function value<T extends Value<any>>(value: T): Promise<T extends Value<infer U> ? U : never> {
+export function value<T extends Value<any, TArgs>, TArgs extends any[] = []>(value: T, ...args: TArgs): Promise<T extends Value<infer U, any> ? U : never> {
   if (typeof value === 'function') {
-    return (value as any)()
+    return (value as any)(...args)
   }
 
   return value as any

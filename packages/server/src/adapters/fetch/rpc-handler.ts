0a12095d803bb37c5f426d86bc770dc3a5966bd8
unnoq <contact@unnoq.com>
2025-02-04 11:06:27 +0700

    rpc handler

---
+++ packages/server/src/adapters/fetch/rpc-handler.ts
@@ -0,0 +1,31 @@
+import type { Context } from '../../context'
+import type { Router } from '../../router'
+import type { RPCHandlerOptions, StandardHandleRest } from '../standard'
+import type { FetchHandler, FetchHandleResult } from './types'
+import { RPCCodec, RPCMatcher, StandardHandler } from '../standard'
+import { fetchRequestToStandardRequest, standardResponseToFetchResponse } from './utils'
+
+export class RPCHandler<T extends Context> implements FetchHandler<T> {
+  private readonly standardHandler: StandardHandler<T>
+
+  constructor(router: Router<T, any>, options?: NoInfer<RPCHandlerOptions<T>>) {
+    const matcher = options?.matcher ?? new RPCMatcher()
+    const codec = options?.codec ?? new RPCCodec()
+    this.standardHandler = new StandardHandler(router, matcher, codec, options)
+  }
+
+  async handle(request: Request, ...rest: StandardHandleRest<T>): Promise<FetchHandleResult> {
+    const standardRequest = fetchRequestToStandardRequest(request)
+
+    const result = await this.standardHandler.handle(standardRequest, ...rest)
+
+    if (!result.matched) {
+      return result
+    }
+
+    return {
+      matched: true,
+      response: standardResponseToFetchResponse(result.response),
+    }
+  }
+}
--- packages/server/src/adapters/fetch/types.ts
+++ packages/server/src/adapters/fetch/types.ts
@@ -1,16 +1,8 @@
-import type { HTTPPath } from '@orpc/contract'
 import type { Context } from '../../context'
-
-export type FetchHandleOptions<T extends Context> =
-  & { prefix?: HTTPPath }
-  & (Record<never, never> extends T ? { context?: T } : { context: T })
-
-export type FetchHandleRest<T extends Context> =
-  | [options: FetchHandleOptions<T>]
-  | (Record<never, never> extends T ? [] : never)
+import type { StandardHandleRest } from '../standard'
 
 export type FetchHandleResult = { matched: true, response: Response } | { matched: false, response: undefined }
 
 export interface FetchHandler<T extends Context> {
-  handle(request: Request, ...rest: FetchHandleRest<T>): Promise<FetchHandleResult>
+  handle(request: Request, ...rest: StandardHandleRest<T>): Promise<FetchHandleResult>
 }

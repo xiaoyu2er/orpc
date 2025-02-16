d84fc7929443fc0ac7903ab8bca09554701c5ffe
unnoq <contact@unnoq.com>
2025-02-16 17:19:44 +0700

    refactor(shared): value utility
    Fix: it not warning when passing invalid args or missing args

--- packages/shared/src/value.ts
+++ packages/shared/src/value.ts
@@ -2,7 +2,10 @@ import type { Promisable } from 'type-fest'
 
 export type Value<T, TArgs extends any[] = []> = T | ((...args: TArgs) => Promisable<T>)
 
-export function value<T extends Value<any, TArgs>, TArgs extends any[] = []>(value: T, ...args: TArgs): Promise<T extends Value<infer U, any> ? U : never> {
+export function value<T, TArgs extends any[]>(
+  value: Value<T, TArgs>,
+  ...args: NoInfer<TArgs>
+): Promise<T extends Value<infer U, any> ? U : never> {
   if (typeof value === 'function') {
     return (value as any)(...args)
   }

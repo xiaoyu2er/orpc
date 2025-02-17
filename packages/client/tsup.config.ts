e4973e97a9ac083400cf3c83244010632ba3333f
unnoq <contact@unnoq.com>
2025-02-17 14:38:21 +0700

    init

--- packages/client/tsup.config.ts
+++ packages/client/tsup.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: {
     index: 'src/index.ts',
+    openapi: 'src/openapi/index.ts',
+    rpc: 'src/rpc/index.ts',
     fetch: 'src/adapters/fetch/index.ts',
   },
   sourcemap: true,

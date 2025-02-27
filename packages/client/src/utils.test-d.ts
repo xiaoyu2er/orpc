4764668d514a8f87e6e2baaaccca02540fa365ba
unnoq <contact@unnoq.com>
2025-02-27 15:45:23 +0700

    feat!: update `safe` utility

--- packages/client/src/utils.test-d.ts
+++ packages/client/src/utils.test-d.ts
@@ -3,24 +3,58 @@ import type { Client, ClientContext } from './types'
 import { isDefinedError } from './error'
 import { safe } from './utils'
 
-it('safe', async () => {
+describe('safe', async () => {
   const client = {} as Client<ClientContext, string, number, Error | ORPCError<'BAD_GATEWAY', { val: string }>>
 
-  const [output, error, isDefined] = await safe(client('123'))
+  it('array style', async () => {
+    const [error, data, isDefined] = await safe(client('123'))
 
-  if (!error) {
-    expectTypeOf(output).toEqualTypeOf<number>()
-  }
+    if (error) {
+      expectTypeOf(error).toEqualTypeOf<Error | ORPCError<'BAD_GATEWAY', { val: string }>>()
+      expectTypeOf(data).toEqualTypeOf<undefined>()
+      expectTypeOf(isDefined).toEqualTypeOf<boolean>()
 
-  if (isDefined) {
-    expectTypeOf(error).toEqualTypeOf<ORPCError<'BAD_GATEWAY', { val: string }>>()
-  }
+      if (isDefinedError(error)) {
+        expectTypeOf(error).toEqualTypeOf<ORPCError<'BAD_GATEWAY', { val: string }>>()
+      }
 
-  if (error) {
-    expectTypeOf(error).toEqualTypeOf<Error | ORPCError<'BAD_GATEWAY', { val: string }>>()
-
-    if (isDefinedError(error)) {
-      expectTypeOf(error).toEqualTypeOf<ORPCError<'BAD_GATEWAY', { val: string }>>()
+      if (isDefined) {
+        expectTypeOf(error).toEqualTypeOf<ORPCError<'BAD_GATEWAY', { val: string }>>()
+      }
+      else {
+        expectTypeOf(error).toEqualTypeOf<Error>()
+      }
     }
-  }
+    else {
+      expectTypeOf(error).toEqualTypeOf<null>()
+      expectTypeOf(data).toEqualTypeOf<number>()
+      expectTypeOf(isDefined).toEqualTypeOf<false>()
+    }
+  })
+
+  it('object style', async () => {
+    const { error, data, isDefined } = await safe(client('123'))
+
+    if (error) {
+      expectTypeOf(error).toEqualTypeOf<Error | ORPCError<'BAD_GATEWAY', { val: string }>>()
+      expectTypeOf(data).toEqualTypeOf<undefined>()
+      expectTypeOf(isDefined).toEqualTypeOf<boolean>()
+
+      if (isDefinedError(error)) {
+        expectTypeOf(error).toEqualTypeOf<ORPCError<'BAD_GATEWAY', { val: string }>>()
+      }
+
+      if (isDefined) {
+        expectTypeOf(error).toEqualTypeOf<ORPCError<'BAD_GATEWAY', { val: string }>>()
+      }
+      else {
+        expectTypeOf(error).toEqualTypeOf<Error>()
+      }
+    }
+    else {
+      expectTypeOf(error).toEqualTypeOf<null>()
+      expectTypeOf(data).toEqualTypeOf<number>()
+      expectTypeOf(isDefined).toEqualTypeOf<false>()
+    }
+  })
 })
--- packages/client/src/utils.test.ts
+++ packages/client/src/utils.test.ts
@@ -3,17 +3,21 @@ import { safe } from './utils'
 
 it('safe', async () => {
   const r1 = await safe(Promise.resolve(1))
-  expect(r1).toEqual([1, undefined, false])
+  expect([...r1]).toEqual([null, 1, false])
+  expect({ ...r1 }).toEqual(expect.objectContaining({ error: null, data: 1, isDefined: false }))
 
   const e2 = new Error('error')
   const r2 = await safe(Promise.reject(e2))
-  expect(r2).toEqual([undefined, e2, false])
+  expect([...r2]).toEqual([e2, undefined, false])
+  expect({ ...r2 }).toEqual(expect.objectContaining({ error: e2, data: undefined, isDefined: false }))
 
   const e3 = new ORPCError('BAD_GATEWAY', { defined: true })
   const r3 = await safe(Promise.reject(e3))
-  expect(r3).toEqual([undefined, e3, true])
+  expect([...r3]).toEqual([e3, undefined, true])
+  expect({ ...r3 }).toEqual(expect.objectContaining({ error: e3, data: undefined, isDefined: true }))
 
   const e4 = new ORPCError('BAD_GATEWAY')
   const r4 = await safe(Promise.reject(e4))
-  expect(r4).toEqual([undefined, e4, false])
+  expect([...r4]).toEqual([e4, undefined, false])
+  expect({ ...r4 }).toEqual(expect.objectContaining({ error: e4, data: undefined, isDefined: false }))
 })

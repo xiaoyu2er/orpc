d926426dba381b6f358e5f0ed373b52ba12efc3e
unnoq <contact@unnoq.com>
2025-03-05 15:45:36 +0700

    rpc-links update

--- packages/client/src/adapters/fetch/rpc-link.ts
+++ packages/client/src/adapters/fetch/rpc-link.ts
@@ -6,16 +6,15 @@ import { LinkFetchClient } from './link-fetch-client'
 
 export interface RPCLinkOptions<T extends ClientContext>
   extends StandardLinkOptions<T>, StandardRPCLinkCodecOptions<T>, LinkFetchClientOptions<T> {
-  linkCodec?: StandardRPCLinkCodec<T>
-  linkClient?: LinkFetchClient<T>
 }
 
 export class RPCLink<T extends ClientContext> implements ClientLink<T> {
   private readonly standardLink: StandardLink<T>
 
   constructor(options: RPCLinkOptions<T>) {
-    const linkCodec = options.linkCodec ?? new StandardRPCLinkCodec(options)
-    const linkClient = options.linkClient ?? new LinkFetchClient(options)
+    const linkCodec = new StandardRPCLinkCodec(options)
+    const linkClient = new LinkFetchClient(options)
+
     this.standardLink = new StandardLink(linkCodec, linkClient, options)
   }
 

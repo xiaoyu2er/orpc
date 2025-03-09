5fe279a979162cb85eda4de7b10365bb748f7667
unnoq <contact@unnoq.com>
2025-03-09 20:39:02 +0700

    improve

--- packages/contract/src/router-client.ts
+++ packages/contract/src/router-client.ts
@@ -1,14 +1,12 @@
 import type { ClientContext } from '@orpc/client'
-import type { Lazy, Lazyable } from './lazy'
+import type { Lazyable } from './lazy'
 import type { ContractProcedure } from './procedure'
 import type { ContractProcedureClient } from './procedure-client'
 import type { AnyContractRouter } from './router'
 
-export type ContractRouterClient<T extends Lazyable<AnyContractRouter>, TClientContext extends ClientContext> =
-  T extends Lazy<infer U extends AnyContractRouter>
-    ? ContractRouterClient<U, TClientContext>
-    : T extends ContractProcedure<infer UInputSchema, infer UOutputSchema, infer UErrorMap, any>
-      ? ContractProcedureClient<TClientContext, UInputSchema, UOutputSchema, UErrorMap>
-      : {
-          [K in keyof T]: T[K] extends Lazyable<AnyContractRouter> ? ContractRouterClient<T[K], TClientContext> : never
-        }
+export type ContractRouterClient<T extends AnyContractRouter, TClientContext extends ClientContext> =
+   T extends ContractProcedure<infer UInputSchema, infer UOutputSchema, infer UErrorMap, any>
+     ? ContractProcedureClient<TClientContext, UInputSchema, UOutputSchema, UErrorMap>
+     : {
+         [K in keyof T]: T[K] extends Lazyable<infer U extends AnyContractRouter> ? ContractRouterClient<U, TClientContext> : never
+       }
--- packages/contract/src/router.ts
+++ packages/contract/src/router.ts
@@ -1,4 +1,4 @@
-import type { Lazy, Lazyable } from './lazy'
+import type { Lazyable } from './lazy'
 import type { Meta } from './meta'
 import type { ContractProcedure } from './procedure'
 import type { SchemaInput, SchemaOutput } from './schema'
@@ -11,30 +11,24 @@ export type ContractRouter<TMeta extends Meta> =
 
 export type AnyContractRouter = ContractRouter<any>
 
-export type InferContractRouterInputs<T extends Lazyable<AnyContractRouter>> =
-  T extends Lazy<infer U extends AnyContractRouter>
-    ? InferContractRouterInputs<U>
-    : T extends ContractProcedure<infer UInputSchema, any, any, any>
-      ? SchemaInput<UInputSchema>
-      : {
-          [K in keyof T]: T[K] extends Lazyable<AnyContractRouter> ? InferContractRouterInputs<T[K]> : never
-        }
+export type InferContractRouterInputs<T extends AnyContractRouter> =
+  T extends ContractProcedure<infer UInputSchema, any, any, any>
+    ? SchemaInput<UInputSchema>
+    : {
+        [K in keyof T]: T[K] extends Lazyable<infer U extends AnyContractRouter> ? InferContractRouterInputs<U> : never
+      }
 
 export type InferContractRouterOutputs<T extends Lazyable<AnyContractRouter>> =
-  T extends Lazy<infer U extends AnyContractRouter>
-    ? InferContractRouterOutputs<U>
-    : T extends ContractProcedure<any, infer UOutputSchema, any, any>
-      ? SchemaOutput<UOutputSchema>
-      : {
-          [K in keyof T]: T[K] extends Lazyable<AnyContractRouter> ? InferContractRouterOutputs<T[K]> : never
-        }
+   T extends ContractProcedure<any, infer UOutputSchema, any, any>
+     ? SchemaOutput<UOutputSchema>
+     : {
+         [K in keyof T]: T[K] extends Lazyable<infer U extends AnyContractRouter> ? InferContractRouterOutputs<U> : never
+       }
 
 export type InterContractRouterErrorMap<T extends Lazyable<AnyContractRouter>> =
-  T extends Lazy<infer U extends AnyContractRouter>
-    ? InterContractRouterErrorMap<U>
-    : T extends ContractProcedure<any, any, infer UErrorMap, any> ? UErrorMap :
-        {
-          [K in keyof T]: T[K] extends Lazyable<AnyContractRouter> ? InterContractRouterErrorMap<T[K]> : never
-        }[keyof T]
+  T extends ContractProcedure<any, any, infer UErrorMap, any> ? UErrorMap :
+      {
+        [K in keyof T]: T[K] extends Lazyable<infer U extends AnyContractRouter> ? InterContractRouterErrorMap<U> : never
+      }[keyof T]
 
 export type InterContractRouterMeta<T extends AnyContractRouter> = T extends ContractRouter<infer UMeta> ? UMeta : never

1607bd109f1a28d2049bca2a9682ab5d5195931a
unnoq <contact@unnoq.com>
2025-03-11 19:27:04 +0700

    feat(server)!: remove dedupe logic from middleware

--- packages/server/src/middleware-utils.test.ts
+++ packages/server/src/middleware-utils.test.ts
@@ -1,25 +1,14 @@
-import { addMiddleware, dedupeMiddlewares, mergeMiddlewares } from './middleware-utils'
+import { addMiddleware, mergeMiddlewares } from './middleware-utils'
 
 const mid1 = vi.fn()
 const mid2 = vi.fn()
 const mid3 = vi.fn()
 
-it('dedupeMiddlewares', () => {
-  expect(dedupeMiddlewares([mid1, mid2], [])).toEqual([])
-  expect(dedupeMiddlewares([], [mid1, mid2])).toEqual([mid1, mid2])
-  expect(dedupeMiddlewares([mid1], [mid2])).toEqual([mid2])
-  expect(dedupeMiddlewares([mid1], [mid1, mid2])).toEqual([mid2])
-  expect(dedupeMiddlewares([mid1, mid3], [mid1, mid2])).toEqual([mid2])
-  expect(dedupeMiddlewares([mid1, mid3], [mid1, mid2, mid3])).toEqual([mid2, mid3])
-})
-
 it('mergeMiddlewares', () => {
   expect(mergeMiddlewares([mid1, mid2], [])).toEqual([mid1, mid2])
   expect(mergeMiddlewares([], [mid1, mid2])).toEqual([mid1, mid2])
   expect(mergeMiddlewares([mid1], [mid2])).toEqual([mid1, mid2])
-  expect(mergeMiddlewares([mid1], [mid1, mid2])).toEqual([mid1, mid2])
-  expect(mergeMiddlewares([mid1, mid3], [mid1, mid2])).toEqual([mid1, mid3, mid2])
-  expect(mergeMiddlewares([mid1, mid3], [mid1, mid2, mid3])).toEqual([mid1, mid3, mid2, mid3])
+  expect(mergeMiddlewares([mid1], [mid1, mid2])).toEqual([mid1, mid1, mid2])
 })
 
 it('addMiddleware', () => {

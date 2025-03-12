482df169676d064dc06696965f97935f830fcc33
unnoq <dinwwwh@gmail.com>
2025-03-12 21:00:18 +0700

    Revert "feat(server)!: remove dedupe middleware logic (#225)"
    This reverts commit bf4983320ddbb5540333ab7c005ed6f329a48eb7.

--- packages/server/src/middleware-utils.test.ts
+++ packages/server/src/middleware-utils.test.ts
@@ -1,14 +1,25 @@
-import { addMiddleware, mergeMiddlewares } from './middleware-utils'
+import { addMiddleware, dedupeMiddlewares, mergeMiddlewares } from './middleware-utils'
 
 const mid1 = vi.fn()
 const mid2 = vi.fn()
 const mid3 = vi.fn()
 
+it('dedupeMiddlewares', () => {
+  expect(dedupeMiddlewares([mid1, mid2], [])).toEqual([])
+  expect(dedupeMiddlewares([], [mid1, mid2])).toEqual([mid1, mid2])
+  expect(dedupeMiddlewares([mid1], [mid2])).toEqual([mid2])
+  expect(dedupeMiddlewares([mid1], [mid1, mid2])).toEqual([mid2])
+  expect(dedupeMiddlewares([mid1, mid3], [mid1, mid2])).toEqual([mid2])
+  expect(dedupeMiddlewares([mid1, mid3], [mid1, mid2, mid3])).toEqual([mid2, mid3])
+})
+
 it('mergeMiddlewares', () => {
   expect(mergeMiddlewares([mid1, mid2], [])).toEqual([mid1, mid2])
   expect(mergeMiddlewares([], [mid1, mid2])).toEqual([mid1, mid2])
   expect(mergeMiddlewares([mid1], [mid2])).toEqual([mid1, mid2])
-  expect(mergeMiddlewares([mid1], [mid1, mid2])).toEqual([mid1, mid1, mid2])
+  expect(mergeMiddlewares([mid1], [mid1, mid2])).toEqual([mid1, mid2])
+  expect(mergeMiddlewares([mid1, mid3], [mid1, mid2])).toEqual([mid1, mid3, mid2])
+  expect(mergeMiddlewares([mid1, mid3], [mid1, mid2, mid3])).toEqual([mid1, mid3, mid2, mid3])
 })
 
 it('addMiddleware', () => {
--- packages/server/src/middleware-utils.ts
+++ packages/server/src/middleware-utils.ts
@@ -1,7 +1,23 @@
 import type { AnyMiddleware } from './middleware'
 
+export function dedupeMiddlewares(compare: readonly AnyMiddleware[], middlewares: readonly AnyMiddleware[]): AnyMiddleware[] {
+  let min = 0
+
+  for (let i = 0; i < middlewares.length; i++) {
+    const index = compare.indexOf(middlewares[i]!, min)
+
+    if (index === -1) {
+      return middlewares.slice(i)
+    }
+
+    min = index + 1
+  }
+
+  return []
+}
+
 export function mergeMiddlewares(first: readonly AnyMiddleware[], second: readonly AnyMiddleware[]): AnyMiddleware[] {
-  return [...first, ...second]
+  return [...first, ...dedupeMiddlewares(first, second)]
 }
 
 export function addMiddleware(middlewares: readonly AnyMiddleware[], addition: AnyMiddleware): AnyMiddleware[] {

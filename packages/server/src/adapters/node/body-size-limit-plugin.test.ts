68325c46a206b8ed179ef2b350a3a068746acf2d
unnoq <contact@unnoq.com>
2025-03-24 16:43:08 +0700

    node

---
+++ packages/server/src/adapters/node/body-size-limit-plugin.test.ts
@@ -0,0 +1,77 @@
+import type { IncomingMessage, ServerResponse } from 'node:http'
+import request from 'supertest'
+import { os } from '../../builder'
+import { RPCHandler as FetchRPChandler } from '../fetch/rpc-handler'
+import { BodySizeLimitPlugin } from './body-size-limit-plugin'
+import { RPCHandler } from './rpc-handler'
+
+describe('bodySizeLimitPlugin', () => {
+  const size22Json = { json: { foo: 'bar' } }
+
+  it('check the content-length', async () => {
+    const res = await request(async (req: IncomingMessage, res: ServerResponse) => {
+      const handler = new RPCHandler(os.handler(() => 'ping'), {
+        plugins: [
+          new BodySizeLimitPlugin({ maxBodySize: 21 }),
+        ],
+      })
+
+      await handler.handle(req, res)
+    })
+      .post('/')
+      .send(size22Json)
+      .set('content-length', '22')
+
+    expect(res.text).toContain('PAYLOAD_TOO_LARGE')
+    expect(res.status).toBe(413)
+  })
+
+  it('check the body-size', async () => {
+    const res = await request(async (req: IncomingMessage, res: ServerResponse) => {
+      const handler = new RPCHandler(os.handler(() => 'ping'), {
+        plugins: [
+          new BodySizeLimitPlugin({ maxBodySize: 21 }),
+        ],
+      })
+
+      delete req.headers['content-length']
+
+      await handler.handle(req, res)
+    })
+      .post('/')
+      .send(size22Json)
+
+    expect(res.text).toContain('PAYLOAD_TOO_LARGE')
+    expect(res.status).toBe(413)
+  })
+
+  it('should work if body size is not exceeded', async () => {
+    const res = await request(async (req: IncomingMessage, res: ServerResponse) => {
+      const handler = new RPCHandler(os.handler(() => 'ping'), {
+        plugins: [
+          new BodySizeLimitPlugin({ maxBodySize: 22 }),
+        ],
+      })
+
+      await handler.handle(req, res)
+    })
+      .post('/')
+      .send(size22Json)
+
+    expect(res.text).toContain('ping')
+    expect(res.status).toBe(200)
+  })
+
+  it('should throw if used outside of node adapter', async () => {
+    const handler = new FetchRPChandler(os.handler(() => 'ping'), {
+      plugins: [
+        new BodySizeLimitPlugin({ maxBodySize: 22 }),
+      ],
+    })
+
+    const { response } = await handler.handle(new Request('https://example.com', { method: 'POST' }))
+
+    await expect(response?.text()).resolves.toContain('INTERNAL_SERVER_ERROR')
+    expect(response?.status).toBe(500)
+  })
+})
---
+++ packages/server/src/adapters/node/body-size-limit-plugin.ts
@@ -0,0 +1,60 @@
+import type { NodeHttpRequest } from '@orpc/standard-server-node'
+import type { Context } from '../../context'
+import type { HandlerPlugin } from '../../plugins'
+import type { StandardHandlerOptions } from '../standard'
+import { ORPCError } from '@orpc/client'
+import { once } from '@orpc/shared'
+
+export interface BodySizeLimitPluginOptions {
+  maxBodySize: number
+}
+
+export class BodySizeLimitPlugin<T extends Context> implements HandlerPlugin<T> {
+  private readonly maxBodySize: number
+
+  constructor(options: BodySizeLimitPluginOptions) {
+    this.maxBodySize = options.maxBodySize
+  }
+
+  init(options: StandardHandlerOptions<T>): void {
+    options.interceptors ??= []
+
+    options.interceptors.unshift((interceptorOptions) => {
+      if (interceptorOptions.request.headers['content-length']) {
+        const contentLength = Number(interceptorOptions.request.headers['content-length'])
+
+        if (contentLength > this.maxBodySize) {
+          throw new ORPCError('PAYLOAD_TOO_LARGE')
+        }
+      }
+
+      return interceptorOptions.next({
+        ...interceptorOptions,
+        request: {
+          ...interceptorOptions.request,
+          body: once(() => {
+            if (interceptorOptions.request.raw.adapter !== 'node') {
+              throw new ORPCError('INTERNAL_SERVER_ERROR', {
+                cause: new Error(`BodySizeLimitPlugin error: expected adapter 'node' but received '${interceptorOptions.request.raw.adapter}'.`),
+              })
+            }
+
+            const req = interceptorOptions.request.raw.request as NodeHttpRequest
+
+            let currentBodySize = 0
+
+            req.on('data', (chunk) => {
+              currentBodySize += chunk.length
+
+              if (currentBodySize > this.maxBodySize) {
+                throw new ORPCError('PAYLOAD_TOO_LARGE')
+              }
+            })
+
+            return interceptorOptions.request.body()
+          }),
+        },
+      })
+    })
+  }
+}

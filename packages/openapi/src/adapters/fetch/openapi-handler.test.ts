358a6a8a518a9268efa50f0dae2b60f466f7c367
unnoq <contact@unnoq.com>
2025-03-25 16:16:27 +0700

    wip

--- packages/openapi/src/adapters/fetch/openapi-handler.test.ts
+++ packages/openapi/src/adapters/fetch/openapi-handler.test.ts
@@ -1,95 +1,15 @@
-import { StandardHandler } from '@orpc/server/standard'
-import { toFetchResponse, toStandardLazyRequest } from '@orpc/standard-server-fetch'
-import { describe, expect, it, vi } from 'vitest'
-import { router } from '../../../../server/tests/shared'
+import { os } from '@orpc/server'
 import { OpenAPIHandler } from './openapi-handler'
 
-vi.mock('@orpc/server/standard', async origin => ({
-  ...await origin(),
-  StandardHandler: vi.fn(),
-}))
-
-vi.mock('@orpc/standard-server-fetch', async origin => ({
-  toStandardLazyRequest: vi.fn((await origin() as any).toStandardLazyRequest),
-  toFetchResponse: vi.fn((await origin() as any).toFetchResponse),
-}))
-
-beforeEach(() => {
-  vi.clearAllMocks()
-})
-
 describe('openAPIHandler', () => {
-  const handle = vi.fn()
+  it('works', async () => {
+    const handler = new OpenAPIHandler(os.route({ method: 'GET' }).handler(() => 'pong'))
 
-  vi.mocked(StandardHandler).mockReturnValue({
-    handle,
-  } as any)
-
-  const handler = new OpenAPIHandler(router)
-
-  const request = new Request('https://example.com/api/v1/users/1', {
-    headers: new Headers({ 'content-type': 'application/json' }),
-    method: 'POST',
-    body: JSON.stringify({ data: { value: '123' }, meta: [] }),
-  })
-
-  it('on match', async () => {
-    handle.mockReturnValueOnce({
-      matched: true,
-      response: {
-        status: 200,
-        headers: {},
-        body: '__body__',
-      },
+    const { response } = await handler.handle(new Request('https://example.com/api/v1/'), {
+      prefix: '/api/v1',
     })
 
-    const options = { prefix: '/api/v1', context: { db: 'postgres' } } as const
-    const result = await handler.handle(request, options)
-
-    expect(result).toEqual({
-      matched: true,
-      response: vi.mocked(toFetchResponse).mock.results[0]!.value,
-    })
-
-    expect(handle).toHaveBeenCalledOnce()
-    expect(handle).toHaveBeenCalledWith(
-      vi.mocked(toStandardLazyRequest).mock.results[0]!.value,
-      options,
-    )
-
-    expect(vi.mocked(toStandardLazyRequest)).toHaveBeenCalledOnce()
-    expect(vi.mocked(toStandardLazyRequest)).toHaveBeenCalledWith(request)
-
-    expect(vi.mocked(toFetchResponse)).toHaveBeenCalledOnce()
-    expect(vi.mocked(toFetchResponse)).toHaveBeenCalledWith({
-      status: 200,
-      headers: {},
-      body: '__body__',
-    }, options)
-  })
-
-  it('on mismatch', async () => {
-    handle.mockReturnValueOnce({
-      matched: false,
-      response: undefined,
-    })
-
-    const result = await handler.handle(request, { prefix: '/api/v1', context: { db: 'postgres' } })
-
-    expect(result).toEqual({
-      matched: false,
-      response: undefined,
-    })
-
-    expect(handle).toHaveBeenCalledOnce()
-    expect(handle).toHaveBeenCalledWith(
-      vi.mocked(toStandardLazyRequest).mock.results[0]!.value,
-      { prefix: '/api/v1', context: { db: 'postgres' } },
-    )
-
-    expect(vi.mocked(toStandardLazyRequest)).toHaveBeenCalledOnce()
-    expect(vi.mocked(toStandardLazyRequest)).toHaveBeenCalledWith(request)
-
-    expect(vi.mocked(toFetchResponse)).not.toHaveBeenCalled()
+    await expect(response?.text()).resolves.toContain('pong')
+    expect(response?.status).toBe(200)
   })
 })

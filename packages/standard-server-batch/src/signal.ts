82b304a5e8aa17245d1ae35e75e8b1b76db2a9a3
unnoq <contact@unnoq.com>
2025-04-03 10:30:07 +0700

    wip

--- packages/standard-server-batch/src/signal.ts
+++ packages/standard-server-batch/src/signal.ts
@@ -1,13 +1,23 @@
 export function toBatchAbortSignal(signals: readonly (AbortSignal | undefined)[]): AbortSignal {
+  const realSignals = signals.filter(signal => signal !== undefined)
+
   const controller = new AbortController()
 
-  const abortedSignals = signals.filter(signal => signal.aborted)
+  const abortedSignals = realSignals.filter(signal => signal.aborted)
 
-  if (abortedSignals.length > 0) {
+  if (abortedSignals.length === realSignals.length) {
     controller.abort()
   }
 
-  signals.forEach(signal => signal.addEventListener('abort', () => controller.abort()))
+  for (const signal of realSignals) {
+    signal.addEventListener('abort', () => {
+      abortedSignals.push(signal)
+
+      if (abortedSignals.length === realSignals.length) {
+        controller.abort()
+      }
+    })
+  }
 
   return controller.signal
 }

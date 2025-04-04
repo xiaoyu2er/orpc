a4c3be6109fed550e888b2e1a51cb21248c572fa
unnoq <contact@unnoq.com>
2025-04-04 10:21:04 +0700

    wip

--- packages/standard-server/src/batch/response.ts
+++ packages/standard-server/src/batch/response.ts
@@ -8,14 +8,47 @@ export interface BatchResponseBodyItem extends StandardResponse {
 export interface ToBatchResponseOptions {
   status: number
   headers: StandardHeaders
-  body: AsyncIterator<BatchResponseBodyItem>
+  responsePromises: Promise<StandardResponse>[]
 }
 
 export function toBatchResponse(options: ToBatchResponseOptions): StandardResponse {
+  const genBody: () => AsyncGenerator<BatchResponseBodyItem> = async function* () {
+    const entries: (Promise<BatchResponseBodyItem> | undefined)[] = options.responsePromises.map(
+      (promise, index) => promise
+        .then(response => ({ ...response, index }))
+        .catch(() => ({
+          index,
+          status: 500,
+          headers: {},
+          body: {
+            defined: false,
+            code: 'INTERNAL_SERVER_ERROR',
+            status: 500,
+            message: 'Something went wrong while processing the batch response',
+            data: { index },
+          },
+        })),
+    )
+
+    while (true) {
+      const promises = entries.filter(entry => !!entry)
+
+      if (!promises.length) {
+        return
+      }
+
+      const fastest = await Promise.race(promises)
+
+      entries[fastest.index] = undefined
+
+      yield fastest
+    }
+  }
+
   return {
     status: options.status,
     headers: options.headers,
-    body: options.body,
+    body: genBody(),
   }
 }
 

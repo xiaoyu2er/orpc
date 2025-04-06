c9c8ce36f24cdb5406f92f60269d378062f3eaf6
unnoq <contact@unnoq.com>
2025-04-06 09:36:48 +0700

    wip

--- packages/shared/src/array.test.ts
+++ packages/shared/src/array.test.ts
@@ -1,8 +1,17 @@
-import { toArray } from './array'
+import { splitInHalf, toArray } from './array'
 
 it('toArray', () => {
   expect(toArray(undefined)).toEqual([])
   expect(toArray(null)).toEqual([])
   expect(toArray(1)).toEqual([1])
   expect(toArray([1])).toEqual([1])
+})
+
+it('splitInHalf', () => {
+  expect(splitInHalf([1, 2, 3, 4, 5])).toEqual([[1, 2, 3], [4, 5]])
+  expect(splitInHalf([1, 2, 3, 4])).toEqual([[1, 2], [3, 4]])
+  expect(splitInHalf([1, 2, 3])).toEqual([[1, 2], [3]])
+  expect(splitInHalf([1, 2])).toEqual([[1], [2]])
+  expect(splitInHalf([1])).toEqual([[1], []])
+  expect(splitInHalf([])).toEqual([[], []])
 })
--- packages/shared/src/array.ts
+++ packages/shared/src/array.ts
@@ -1,3 +1,8 @@
 export function toArray<T>(value: T | T[] | null | undefined): T[] {
   return Array.isArray(value) ? value : value === undefined || value === null ? [] : [value]
 }
+
+export function splitInHalf<T>(arr: readonly T[]): [T[], T[]] {
+  const half = Math.ceil(arr.length / 2)
+  return [arr.slice(0, half), arr.slice(half)]
+}

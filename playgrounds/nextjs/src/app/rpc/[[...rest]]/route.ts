199cbf9ceb97e51b0e73131aeb73b26ac6c5dd28
unnoq <contact@unnoq.com>
2025-04-07 17:27:04 +0700

    wip

--- playgrounds/nextjs/src/app/rpc/[[...rest]]/route.ts
+++ playgrounds/nextjs/src/app/rpc/[[...rest]]/route.ts
@@ -2,12 +2,16 @@ import { router } from '@/router'
 import { onError } from '@orpc/server'
 import { RPCHandler, serve } from '@orpc/server/next'
 import '../../../polyfill'
+import { BatchHandlerPlugin } from '@orpc/server/plugins'
 
 const rpcHandler = new RPCHandler(router, {
   interceptors: [
     onError((error) => {
       console.error(error)
     }),
+  ],
+  plugins: [
+    new BatchHandlerPlugin(),
   ],
 })
 

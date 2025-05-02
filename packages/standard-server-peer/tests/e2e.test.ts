01cee674ec4e4f645fa6e65dde78d86d9408565a
unnoq <contact@unnoq.com>
2025-05-02 08:35:40 +0700

    e2e tests

--- packages/standard-server-peer/tests/e2e.test.ts
+++ packages/standard-server-peer/tests/e2e.test.ts
@@ -1,11 +1,16 @@
+import { getEventMeta, withEventMeta } from '@orpc/standard-server'
 import { StandardRPCJsonSerializer } from '../../client/src/adapters/standard/rpc-json-serializer'
 import { StandardRPCSerializer } from '../../client/src/adapters/standard/rpc-serializer'
 import { supportedDataTypes } from '../../client/tests/shared'
 import { ClientPeer, ServerPeer } from '../src'
 
-describe.each(supportedDataTypes)('types: %s', ({ value, expected }) => {
+describe('client/server peer', () => {
+  let server: ServerPeer
+
   const client = new ClientPeer(async (raw) => {
-    const server = new ServerPeer(client.message.bind(client))
+    server ??= new ServerPeer(async (raw) => {
+      await client.message(typeof raw === 'string' ? Math.random() < 0.5 ? raw : new TextEncoder().encode(raw) : raw)
+    })
 
     const [id, request] = await server.message(typeof raw === 'string' ? Math.random() < 0.5 ? raw : new TextEncoder().encode(raw) : raw)
 
@@ -13,7 +18,7 @@ describe.each(supportedDataTypes)('types: %s', ({ value, expected }) => {
       return
     }
 
-    await server.response(id, {
+    server.response(id, {
       status: 200,
       ...request,
     })
@@ -21,45 +26,87 @@ describe.each(supportedDataTypes)('types: %s', ({ value, expected }) => {
 
   const serializer = new StandardRPCSerializer(new StandardRPCJsonSerializer())
 
-  async function assert(value: unknown, expected: unknown): Promise<void> {
+  describe.each(supportedDataTypes)('types: %s', ({ value, expected }) => {
+    async function assert(value: unknown, expected: unknown): Promise<void> {
+      const response = await client.request({
+        headers: {},
+        method: 'POST',
+        signal: undefined,
+        url: new URL('http://localhost:3000'),
+        body: serializer.serialize(value),
+      })
+
+      const output = await serializer.deserialize(response.body)
+
+      expect(output).toEqual(expected)
+    }
+
+    it('should work on flat', async () => {
+      await assert(value, expected)
+    })
+
+    it('should work on nested object', async () => {
+      await assert({ data: value }, { data: expected })
+    })
+
+    it('should work on complex object', async () => {
+      await assert({
+        '!@#$%^^&()[]>?<~_<:"~+!_': value,
+        'list': [value],
+        'map': new Map([[value, value]]),
+        'set': new Set([value]),
+        'nested': {
+          nested: value,
+        },
+      }, {
+        '!@#$%^^&()[]>?<~_<:"~+!_': expected,
+        'list': [expected],
+        'map': new Map([[expected, expected]]),
+        'set': new Set([expected]),
+        'nested': {
+          nested: expected,
+        },
+      })
+    })
+  })
+
+  it('event iterator', async () => {
     const response = await client.request({
       headers: {},
       method: 'POST',
       signal: undefined,
       url: new URL('http://localhost:3000'),
-      body: serializer.serialize(value),
+      body: serializer.serialize((async function* () {
+        yield 'hello1'
+        yield withEventMeta({ hello2: true }, { id: 'id-1' })
+        return withEventMeta({ hello3: true }, { id: 'id-2' })
+      })()),
     })
 
-    const output = await serializer.deserialize(response.body)
+    const output = await serializer.deserialize(response.body) as AsyncGenerator
 
-    expect(output).toEqual(expected)
-  }
+    expect(await output.next()).toSatisfy(({ done, value }) => {
+      expect(done).toBe(false)
+      expect(value).toEqual('hello1')
+      expect(getEventMeta(value)).toEqual(undefined)
 
-  it('should work on flat', async () => {
-    await assert(value, expected)
-  })
+      return true
+    })
 
-  it('should work on nested object', async () => {
-    await assert({ data: value }, { data: expected })
-  })
+    expect(await output.next()).toSatisfy(({ done, value }) => {
+      expect(done).toBe(false)
+      expect(value).toEqual({ hello2: true })
+      expect(getEventMeta(value)).toEqual({ id: 'id-1' })
 
-  it('should work on complex object', async () => {
-    await assert({
-      '!@#$%^^&()[]>?<~_<:"~+!_': value,
-      'list': [value],
-      'map': new Map([[value, value]]),
-      'set': new Set([value]),
-      'nested': {
-        nested: value,
-      },
-    }, {
-      '!@#$%^^&()[]>?<~_<:"~+!_': expected,
-      'list': [expected],
-      'map': new Map([[expected, expected]]),
-      'set': new Set([expected]),
-      'nested': {
-        nested: expected,
-      },
+      return true
+    })
+
+    expect(await output.next()).toSatisfy(({ done, value }) => {
+      expect(done).toBe(true)
+      expect(value).toEqual({ hello3: true })
+      expect(getEventMeta(value)).toEqual({ id: 'id-2' })
+
+      return true
     })
   })
 })

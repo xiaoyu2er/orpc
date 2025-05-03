361fb604085f125671a8d6215b9dac532175a67c
unnoq <contact@unnoq.com>
2025-05-03 19:56:52 +0700

    wip

---
+++ packages/zod/src/v4/registries.ts
@@ -0,0 +1,60 @@
+import type { JSONSchema } from '@orpc/openapi'
+import type { $input, $output } from '@zod/core'
+import { registry } from '@zod/core'
+
+/**
+ * Zod registry for customizing generated JSON schema, can use both for .input and .output
+ *
+ * @example
+ * ```ts
+ * import { JSON_SCHEMA_REGISTRY } from '@orpc/zod/v4'
+ *
+ * const user = z.object({
+ *   name: z.string(),
+ *   age: z.number(),
+ * })
+ *
+ * JSON_SCHEMA_REGISTRY.add(user, {
+ *   examples: [{ name: 'John', age: 20 }],
+ * })
+ * ```
+ */
+export const JSON_SCHEMA_REGISTRY = registry<Exclude<JSONSchema<$input | $output>, boolean>>()
+
+/**
+ * Zod registry for customizing generated JSON schema, only useful for .input
+ *
+ * @example
+ * ```ts
+ * import { JSON_SCHEMA_INPUT_REGISTRY } from '@orpc/zod/v4'
+ *
+ * const user = z.object({
+ *   name: z.string(),
+ *   age: z.string().transform(v => Number(v)),
+ * })
+ *
+ * JSON_SCHEMA_REGISTRY.add(user, {
+ *   examples: [{ name: 'John', age: "20" }],
+ * })
+ * ```
+ */
+export const JSON_SCHEMA_INPUT_REGISTRY = registry<Exclude<JSONSchema<$input>, boolean>>()
+
+/**
+ * Zod registry for customizing generated JSON schema, only useful for .input
+ *
+ * @example
+ * ```ts
+ * import { JSON_SCHEMA_OUTPUT_REGISTRY } from '@orpc/zod/v4'
+ *
+ * const user = z.object({
+ *   name: z.string(),
+ *   age: z.string().transform(v => Number(v)),
+ * })
+ *
+ * JSON_SCHEMA_OUTPUT_REGISTRY.add(user, {
+ *   examples: [{ name: 'John', age: 20 }],
+ * })
+ * ```
+ */
+export const JSON_SCHEMA_OUTPUT_REGISTRY = registry<Exclude<JSONSchema<$output>, boolean>>()

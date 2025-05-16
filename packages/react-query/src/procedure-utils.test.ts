16bb7e7b74784436e5a1420560cc566a246021ce
unnoq <contact@unnoq.com>
2025-05-16 08:48:00 +0700

    react

--- packages/react-query/src/procedure-utils.test.ts
+++ packages/react-query/src/procedure-utils.test.ts
@@ -1,3 +1,4 @@
+import { skipToken } from '@tanstack/react-query'
 import * as Key from './key'
 import { createProcedureUtils } from './procedure-utils'
 
@@ -17,38 +18,80 @@ describe('createProcedureUtils', () => {
     expect(utils.call).toBe(client)
   })
 
-  it('.queryOptions', async () => {
-    const options = utils.queryOptions({ input: { search: '__search__' }, context: { batch: '__batch__' } })
+  describe('.queryOptions', () => {
+    it('without skipToken', async () => {
+      const options = utils.queryOptions({ input: { search: '__search__' }, context: { batch: '__batch__' } })
 
-    expect(options.queryKey).toBe(buildKeySpy.mock.results[0]!.value)
-    expect(buildKeySpy).toHaveBeenCalledTimes(1)
-    expect(buildKeySpy).toHaveBeenCalledWith(['ping'], { type: 'query', input: { search: '__search__' } })
+      expect(options.enabled).toBe(true)
 
-    await expect(options.queryFn!({ signal } as any)).resolves.toEqual('__output__')
-    expect(client).toHaveBeenCalledTimes(1)
-    expect(client).toBeCalledWith({ search: '__search__' }, { signal, context: { batch: '__batch__' } })
-  })
+      expect(options.queryKey).toBe(buildKeySpy.mock.results[0]!.value)
+      expect(buildKeySpy).toHaveBeenCalledTimes(1)
+      expect(buildKeySpy).toHaveBeenCalledWith(['ping'], { type: 'query', input: { search: '__search__' } })
 
-  it('.infiniteOptions', async () => {
-    const getNextPageParam = vi.fn()
-
-    const options = utils.infiniteOptions({
-      input: pageParam => ({ search: '__search__', pageParam }),
-      context: { batch: '__batch__' },
-      getNextPageParam,
-      initialPageParam: '__initialPageParam__',
+      await expect(options.queryFn!({ signal } as any)).resolves.toEqual('__output__')
+      expect(client).toHaveBeenCalledTimes(1)
+      expect(client).toBeCalledWith({ search: '__search__' }, { signal, context: { batch: '__batch__' } })
     })
 
-    expect(options.queryKey).toBe(buildKeySpy.mock.results[0]!.value)
-    expect(buildKeySpy).toHaveBeenCalledTimes(1)
-    expect(buildKeySpy).toHaveBeenCalledWith(['ping'], { type: 'infinite', input: { search: '__search__', pageParam: '__initialPageParam__' } })
+    it('with skipToken', async () => {
+      const options = utils.queryOptions({ input: skipToken, context: { batch: '__batch__' } })
 
-    expect(options.initialPageParam).toEqual('__initialPageParam__')
-    expect(options.getNextPageParam).toBe(getNextPageParam)
+      expect(options.enabled).toBe(false)
 
-    await expect(options.queryFn!({ signal, pageParam: '__pageParam__' } as any)).resolves.toEqual('__output__')
-    expect(client).toHaveBeenCalledTimes(1)
-    expect(client).toBeCalledWith({ search: '__search__', pageParam: '__pageParam__' }, { signal, context: { batch: '__batch__' } })
+      expect(options.queryKey).toBe(buildKeySpy.mock.results[0]!.value)
+      expect(buildKeySpy).toHaveBeenCalledTimes(1)
+      expect(buildKeySpy).toHaveBeenCalledWith(['ping'], { type: 'query', input: skipToken })
+
+      expect(() => options.queryFn!({ signal } as any)).toThrow('queryFn should not be called with skipToken used as input')
+    })
+  })
+
+  describe('.infiniteOptions', () => {
+    it('without skipToken', async () => {
+      const getNextPageParam = vi.fn()
+
+      const options = utils.infiniteOptions({
+        input: pageParam => ({ search: '__search__', pageParam }),
+        context: { batch: '__batch__' },
+        getNextPageParam,
+        initialPageParam: '__initialPageParam__',
+      })
+
+      expect(options.enabled).toBe(true)
+
+      expect(options.queryKey).toBe(buildKeySpy.mock.results[0]!.value)
+      expect(buildKeySpy).toHaveBeenCalledTimes(1)
+      expect(buildKeySpy).toHaveBeenCalledWith(['ping'], { type: 'infinite', input: { search: '__search__', pageParam: '__initialPageParam__' } })
+
+      expect(options.initialPageParam).toEqual('__initialPageParam__')
+      expect(options.getNextPageParam).toBe(getNextPageParam)
+
+      await expect(options.queryFn!({ signal, pageParam: '__pageParam__' } as any)).resolves.toEqual('__output__')
+      expect(client).toHaveBeenCalledTimes(1)
+      expect(client).toBeCalledWith({ search: '__search__', pageParam: '__pageParam__' }, { signal, context: { batch: '__batch__' } })
+    })
+
+    it('with skipToken', async () => {
+      const getNextPageParam = vi.fn()
+
+      const options = utils.infiniteOptions({
+        input: skipToken,
+        context: { batch: '__batch__' },
+        getNextPageParam,
+        initialPageParam: '__initialPageParam__',
+      })
+
+      expect(options.enabled).toBe(false)
+
+      expect(options.queryKey).toBe(buildKeySpy.mock.results[0]!.value)
+      expect(buildKeySpy).toHaveBeenCalledTimes(1)
+      expect(buildKeySpy).toHaveBeenCalledWith(['ping'], { type: 'infinite', input: skipToken })
+
+      expect(options.initialPageParam).toEqual('__initialPageParam__')
+      expect(options.getNextPageParam).toBe(getNextPageParam)
+
+      expect(() => options.queryFn!({ signal, pageParam: '__pageParam__' } as any)).toThrow('queryFn should not be called with skipToken used as input')
+    })
   })
 
   it('.mutationOptions', async () => {

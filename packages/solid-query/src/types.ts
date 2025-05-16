c8044190952e0af7d25335d790436a56cb9bead4
unnoq <contact@unnoq.com>
2025-05-16 09:39:29 +0700

    solid

--- packages/solid-query/src/types.ts
+++ packages/solid-query/src/types.ts
@@ -3,13 +3,14 @@ import type { SetOptional } from '@orpc/shared'
 import type {
   QueryFunctionContext,
   QueryKey,
+  SkipToken,
   SolidInfiniteQueryOptions,
   SolidMutationOptions,
   SolidQueryOptions,
 } from '@tanstack/solid-query'
 
 export type QueryOptionsIn<TClientContext extends ClientContext, TInput, TOutput, TError, TSelectData> =
-  & (undefined extends TInput ? { input?: TInput } : { input: TInput })
+  & (undefined extends TInput ? { input?: TInput | SkipToken } : { input: TInput | SkipToken })
   & (Record<never, never> extends TClientContext ? { context?: TClientContext } : { context: TClientContext })
   & SetOptional<SolidQueryOptions<TOutput, TError, TSelectData>, 'queryKey'>
 
@@ -17,10 +18,11 @@ export interface QueryOptionsBase<TOutput, TError> {
   queryKey: QueryKey
   queryFn(ctx: QueryFunctionContext): Promise<TOutput>
   retry?(failureCount: number, error: TError): boolean // this make tanstack can infer the TError type
+  enabled: boolean
 }
 
 export type InfiniteOptionsIn<TClientContext extends ClientContext, TInput, TOutput, TError, TSelectData, TPageParam> =
-  & { input: (pageParam: TPageParam) => TInput }
+  & { input: ((pageParam: TPageParam) => TInput) | SkipToken }
   & (Record<never, never> extends TClientContext ? { context?: TClientContext } : { context: TClientContext })
   & SetOptional<SolidInfiniteQueryOptions<TOutput, TError, TSelectData, TOutput, QueryKey, TPageParam>, 'queryKey'>
 
@@ -28,6 +30,7 @@ export interface InfiniteOptionsBase<TOutput, TError, TPageParam> {
   queryKey: QueryKey
   queryFn(ctx: QueryFunctionContext<QueryKey, TPageParam>): Promise<TOutput>
   retry?(failureCount: number, error: TError): boolean // this make tanstack can infer the TError type
+  enabled: boolean
 }
 
 export type MutationOptionsIn<TClientContext extends ClientContext, TInput, TOutput, TError, TMutationContext> =

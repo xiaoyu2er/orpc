c4ee6fa945727c0e61f2c1c8658c57b04579afda
unnoq <contact@unnoq.com>
2025-05-21 20:05:11 +0700

    playground + docs

---
+++ playgrounds/browser-extension/entrypoints/background/index.ts
@@ -0,0 +1,10 @@
+import { experimental_RPCHandler as RPCHandler } from '@orpc/server/message-port'
+import { router } from './router'
+
+const handler = new RPCHandler(router)
+
+export default defineBackground(() => {
+  browser.runtime.onConnect.addListener((port) => {
+    handler.upgrade(port)
+  })
+})

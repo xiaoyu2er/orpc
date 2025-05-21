f7820e646ea1e291b99a6402caac7a5609051836
unnoq <contact@unnoq.com>
2025-05-21 16:03:49 +0700

    wip

--- packages/client/src/adapters/message-port/message-port.ts
+++ packages/client/src/adapters/message-port/message-port.ts
@@ -1,17 +1,81 @@
-export type SupportedMessagePort = Pick<MessagePort, 'addEventListener' | 'postMessage'>
-
-export type SupportedMessagePortData = string | ArrayBufferLike
-
-export function onMessagePortMessage(port: SupportedMessagePort, callback: (data: SupportedMessagePortData) => void): void {
-  port.addEventListener('message', async (event) => {
-    callback(event.data)
-  })
+/**
+ * The message port used by electron in main process
+ */
+export interface MessagePortMainLike {
+  on: (event: string, callback: (event?: { data: any }) => void) => void
+  postMessage: (data: any) => void
 }
+
+/**
+ * The message port used by browser extension
+ */
+export interface BrowserPortLike {
+  onMessage: {
+    addListener: (callback: (data: any) => void) => void
+  }
+  onDisconnect: {
+    addListener: (callback: () => void) => void
+  }
+  postMessage: (data: any) => void
+}
+
+export type SupportedMessagePort = Pick<MessagePort, 'addEventListener' | 'postMessage'> | MessagePortMainLike | BrowserPortLike
+
+/**
+ *  Message port can support [The structured clone algorithm](https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm)
+ */
+export type SupportedMessagePortData = string | ArrayBufferLike
 
 export function postMessagePortMessage(port: SupportedMessagePort, data: SupportedMessagePortData): void {
   port.postMessage(data)
 }
 
+export function onMessagePortMessage(port: SupportedMessagePort, callback: (data: SupportedMessagePortData) => void): void {
+  /**
+   * MessagePort in node.js might have "on" method but it pass different parameters vs MessagePortMainLike
+   * So we need check "addEventListener" before "on"
+   */
+  if ('addEventListener' in port) {
+    port.addEventListener('message', (event) => {
+      callback(event.data)
+    })
+  }
+  else if ('on' in port) {
+    port.on('message', (event) => {
+      callback(event?.data)
+    })
+  }
+  else if ('onMessage' in port) {
+    port.onMessage.addListener((data) => {
+      callback(data)
+    })
+  }
+  else {
+    throw new Error('Cannot find a addEventListener/on/onMessage method on the port')
+  }
+}
+
 export function onMessagePortClose(port: SupportedMessagePort, callback: () => void): void {
-  port.addEventListener('close', callback)
+  /**
+   * MessagePort in node.js might have "on" method but it pass different parameters vs MessagePortMainLike
+   * So we need check "addEventListener" before "on"
+   */
+  if ('addEventListener' in port) {
+    port.addEventListener('close', async () => {
+      callback()
+    })
+  }
+  else if ('on' in port) {
+    port.on('close', async () => {
+      callback()
+    })
+  }
+  else if ('onDisconnect' in port) {
+    port.onDisconnect.addListener(() => {
+      callback()
+    })
+  }
+  else {
+    throw new Error('Cannot find a addEventListener/on/onDisconnect method on the port')
+  }
 }

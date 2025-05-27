caec8b3e59ac2a3ae24937650bc97010d44d618c
unnoq <contact@unnoq.com>
2025-05-27 10:17:38 +0700

    wip

--- packages/standard-server-aws-lambda/src/index.ts
+++ packages/standard-server-aws-lambda/src/index.ts
@@ -5,6 +5,7 @@ export * from './event-iterator'
 export * from './headers'
 export * from './request'
 export * from './response'
+export * from './signal'
 export * from './url'
 
 export type { APIGatewayEvent } from 'aws-lambda'

c58d6c4030e571d9ccb9d1d069e86446e1dea4fb
unnoq <contact@unnoq.com>
2025-06-07 10:41:02 +0700

    feat(client): reexport `EventPublisher`

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -5,6 +5,6 @@ export * from './event-iterator'
 export * from './types'
 export * from './utils'
 
-export { onError, onFinish, onStart, onSuccess } from '@orpc/shared'
-export type { Registry, ThrowableError } from '@orpc/shared'
+export { EventPublisher, onError, onFinish, onStart, onSuccess } from '@orpc/shared'
+export type { EventPublisherOptions, EventPublisherSubscribeIteratorOptions, Registry, ThrowableError } from '@orpc/shared'
 export { ErrorEvent } from '@orpc/standard-server'

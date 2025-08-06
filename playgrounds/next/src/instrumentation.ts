51063566a869e038d6e7c157bce60b3217601dbc
unnoq <contact@unnoq.com>
2025-08-06 11:11:28 +0700

    remove otel from next playground

--- playgrounds/next/src/instrumentation.ts
+++ playgrounds/next/src/instrumentation.ts
@@ -1,13 +1,3 @@
-import { registerOTel } from '@vercel/otel'
-import { ORPCInstrumentation } from '@orpc/otel'
-
 export async function register() {
-  registerOTel({
-    serviceName: 'next-app',
-    instrumentations: [
-      new ORPCInstrumentation(),
-    ],
-  })
-
   await import('./lib/orpc.server')
 }

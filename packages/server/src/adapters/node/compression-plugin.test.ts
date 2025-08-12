47f2935fda9993b7fd96ee0c4c63ca55f2d5c4b5
unnoq <contact@unnoq.com>
2025-08-12 16:37:30 +0700

    CompressionPlugin - node adapter

---
+++ packages/server/src/adapters/node/compression-plugin.test.ts
@@ -0,0 +1,140 @@
+import type { IncomingMessage, ServerResponse } from 'node:http'
+import request from 'supertest'
+import { os } from '../../builder'
+import { CompressionPlugin } from './compression-plugin'
+import { RPCHandler } from './rpc-handler'
+
+describe('compressionPlugin', () => {
+  const output = 'x'.repeat(1024) // Large enough to trigger compression
+
+  it('should compress response when accept-encoding includes gzip', async () => {
+    const res = await request(async (req: IncomingMessage, res: ServerResponse) => {
+      const handler = new RPCHandler(os.handler(() => output), {
+        plugins: [
+          new CompressionPlugin(),
+        ],
+      })
+
+      await handler.handle(req, res)
+    })
+      .post('/')
+      .set('accept-encoding', 'gzip, deflate')
+      .send({ input: 'test' })
+
+    expect(res.status).toBe(200)
+    expect(res.headers['content-encoding']).toBe('gzip')
+    // Just check that we get a response, not trying to decompress for now
+    expect(res.body).toBeDefined()
+  })
+
+  it('should not compress response when accept-encoding does not include compression', async () => {
+    const res = await request(async (req: IncomingMessage, res: ServerResponse) => {
+      const handler = new RPCHandler(os.handler(() => output), {
+        plugins: [
+          new CompressionPlugin(),
+        ],
+      })
+
+      await handler.handle(req, res)
+    })
+      .post('/')
+      .set('accept-encoding', 'identity')
+      .send({ input: 'test' })
+
+    expect(res.status).toBe(200)
+    expect(res.headers['content-encoding']).toBeUndefined()
+    expect(res.text).toContain(output)
+  })
+
+  it('should not compress responses if not satisfying filter', async () => {
+    const res = await request(async (req: IncomingMessage, res: ServerResponse) => {
+      const handler = new RPCHandler(os.handler(() => output), {
+        plugins: [
+          new CompressionPlugin({
+            filter: () => false, // Disable compression entirely for this test
+          }),
+        ],
+      })
+
+      await handler.handle(req, res)
+    })
+      .post('/')
+      .set('accept-encoding', 'gzip, deflate')
+      .send({ input: 'test' })
+
+    expect(res.status).toBe(200)
+    expect(res.headers['content-encoding']).toBeUndefined()
+    expect(res.text).toContain(output)
+  })
+
+  it('should work with deflate compression', async () => {
+    const res = await request(async (req: IncomingMessage, res: ServerResponse) => {
+      const handler = new RPCHandler(os.handler(() => output), {
+        plugins: [
+          new CompressionPlugin(),
+        ],
+      })
+
+      await handler.handle(req, res)
+    })
+      .post('/')
+      .set('accept-encoding', 'deflate')
+      .send({ input: 'test' })
+
+    expect(res.status).toBe(200)
+    expect(res.headers['content-encoding']).toBe('deflate')
+  })
+
+  it('should throw if rootInterceptor throws', async () => {
+    const res = await request(async (req: IncomingMessage, res: ServerResponse) => {
+      const rootInterceptors: any[] = []
+      const handler = new RPCHandler(os.handler(() => output), {
+        plugins: [
+          new CompressionPlugin(),
+        ],
+        rootInterceptors,
+      })
+
+      // ensure rootInterceptor run after CompressionPlugin
+      rootInterceptors.push(async () => {
+        throw new Error('Test error')
+      })
+
+      await expect(handler.handle(req, res)).rejects.toThrow('Test error')
+
+      res.statusCode = 500
+      res.end()
+    })
+      .post('/')
+      .set('accept-encoding', 'gzip, deflate')
+      .send({ input: 'test' })
+
+    expect(res.status).toBe(500)
+    expect(res.headers['content-encoding']).toBeUndefined()
+  })
+
+  it('should throw if compression options throw', async () => {
+    const res = await request(async (req: IncomingMessage, res: ServerResponse) => {
+      const handler = new RPCHandler(os.handler(() => output), {
+        plugins: [
+          new CompressionPlugin({
+            filter: () => {
+              throw new Error('Test error')
+            },
+          }),
+        ],
+      })
+
+      await expect(handler.handle(req, res)).rejects.toThrow('Test error')
+
+      res.statusCode = 500
+      res.end()
+    })
+      .post('/')
+      .set('accept-encoding', 'gzip, deflate')
+      .send({ input: 'test' })
+
+    expect(res.status).toBe(500)
+    expect(res.headers['content-encoding']).toBeUndefined()
+  })
+})
---
+++ packages/server/src/adapters/node/compression-plugin.ts
@@ -0,0 +1,68 @@
+import type { Context } from '../../context'
+import type { NodeHttpHandlerOptions } from './handler'
+import type { NodeHttpHandlerPlugin } from './plugin'
+import compression from 'compression'
+
+export interface CompressionPluginOptions extends compression.CompressionOptions {
+}
+/**
+ * The Compression Plugin adds response compression to the Node.js HTTP Server.
+ *
+ * @see {@link https://orpc.unnoq.com/docs/plugins/compression Compression Plugin Docs}
+ */
+export class CompressionPlugin<T extends Context> implements NodeHttpHandlerPlugin<T> {
+  private readonly compressionHandler: ReturnType<typeof compression>
+
+  constructor(options: CompressionPluginOptions = {}) {
+    this.compressionHandler = compression(options)
+  }
+
+  initRuntimeAdapter(options: NodeHttpHandlerOptions<T>): void {
+    options.adapterInterceptors ??= []
+    options.adapterInterceptors.push(async (options) => {
+      let resolve: (value: Awaited<ReturnType<typeof options.next>>) => void
+      let reject: (reason?: any) => void
+      const promise = new Promise<Awaited<ReturnType<typeof options.next>>>((res, rej) => {
+        resolve = res
+        reject = rej
+      })
+
+      /**
+       * These methods are proxied by the compression handler, so we need to
+       * store the original methods to call them after compression is done
+       * to prevent side effects to other code outside of this plugin.
+       */
+      const originalWrite = options.response.write
+      const originalEnd = options.response.end
+      const originalOn = options.response.on
+
+      this.compressionHandler(
+        options.request as any,
+        options.response as any,
+        async (err) => {
+          /* v8 ignore next 3 - this never happen in realtime: https://github.com/expressjs/compression/blob/master/index.js#L243 */
+          if (err) {
+            reject(err)
+          }
+          else {
+            try {
+              resolve(await options.next())
+            }
+            catch (error) {
+              reject(error)
+            }
+          }
+        },
+      )
+
+      try {
+        return await promise
+      }
+      finally {
+        options.response.write = originalWrite
+        options.response.end = originalEnd
+        options.response.on = originalOn
+      }
+    })
+  }
+}
